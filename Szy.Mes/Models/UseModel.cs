adea561e455905cfb3a138cd298b83fee8427d31
szy <szy@FYSPCW0018>
2025-12-24 20:32:51 +0800

    添加项目文件。

---
+++ Szy.Mes/Models/UseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Szy.Mes.Models
+{
+    /// <summary>
+    /// 系统用户
+    /// </summary>
+    public class SysUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } // 用户名
+        public string Password { get; set; } // 密码
+        public string RealName { get; set; } // 真实姓名
+        public DateTime CreateTime { get; set; } = DateTime.Now; // 创建时间
+    }
+}
