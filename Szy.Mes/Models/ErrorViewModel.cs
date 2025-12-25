077068cd22cdd75a0e2122689626d81f5dd0faba
deitysun <1357404973@qq.com>
2025-12-25 16:37:07 +0800

    1

--- Szy.Mes/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Szy.Mes.Data
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
